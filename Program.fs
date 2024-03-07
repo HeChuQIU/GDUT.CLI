a5b5463526ca590719f805b8472c30ef7c42daec
HeChu <2296282722@qq.com>
2024-03-07 16:51:25 +0800

    init

--- Program.fs
+++ Program.fs
@@ -1,2 +1,76 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Net
+open GDUT.Auth
+open GDUT.CLI.Utils
+open GDUT.ClassSchedule
+
+let getTimestamp () =
+    DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds()
+
+let exitApplication code = Environment.Exit code
+
+let loginEhall (cookiesSetter: CookieCollection -> unit) =
+    let username = askToInputValue "学号"
+    let password = askToInputSecretValue "密码"
+    let result = Ehall.login username password getTimestamp |> Async.RunSynchronously
+
+    match result with
+    | Error errorValue -> printfnWithColor ConsoleColor.Red $"登录失败: {errorValue}"
+    | Ok resultValue ->
+        printfnWithColor ConsoleColor.Green "登录成功"
+        cookiesSetter <| resultValue
+
+let isLoginEhall (cookies: CookieCollection) =
+    match Ehall.isLogin cookies with
+    | Ok resultValue -> resultValue
+    | Error _ -> false
+
+let getSchedule (cookies: CookieCollection) =
+    let result = getClassSchedule cookies "202302"
+
+    match result with
+    | Error errorValue -> printfnWithColor ConsoleColor.Red $"获取课表失败: {errorValue}"
+    | Ok resultValue ->
+        printfnWithColor ConsoleColor.Green "获取课表成功:"
+
+        resultValue
+        |> Seq.iter (fun x -> printfnWithColor ConsoleColor.Green $"{x}")
+
+
+let rec main (cookies: CookieCollection) =
+    let mutable newCookies = cookies
+    let setCookies cookies = newCookies <- cookies
+
+    let baseChoices = [| ("退出程序", (fun _ -> exitApplication 0)) |]
+
+    let login = isLoginEhall cookies
+
+    let choices =
+        match login with
+        | false -> Array.append baseChoices [| ("登录", (fun _ -> loginEhall setCookies)) |]
+        | true ->
+            Array.append
+                baseChoices
+                [| ("注销", (fun _ -> setCookies <| CookieCollection()))
+                   ("获取课表", (fun _ -> getSchedule cookies)) |]
+
+
+    printfn ""
+
+    printfnWithColor
+        (if login then ConsoleColor.Green else ConsoleColor.Red)
+        $"""当前登录状态: {if login then "已登录" else "未登录"}"""
+
+    choices |> Seq.iteri (fun i (text, _) -> printfn $"%d{i}. %s{text}")
+    let input = askToInputValue "序号"
+
+    match Int32.TryParse input with
+    | true, i when 0 <= i && i < choices.Length ->
+        let _, f = choices[i]
+        printfn ""
+        f ()
+    | _ -> printfnWithColor ConsoleColor.Red "输入无效"
+
+    main newCookies
+
+exit <| (main <| CookieCollection())
--- Utils.fs
+++ Utils.fs
@@ -1,2 +1,52 @@
 module GDUT.CLI.Utils
 
+open System
+
+let printfnWithColor color text =
+    Console.ForegroundColor <- color
+    printfn text
+    Console.ResetColor()
+
+let rec askToInputValue name =
+    printfn $"请输入%s{name}:"
+    let input = Console.ReadLine()
+    match input with
+    | _ when String.IsNullOrEmpty input ->
+        printfnWithColor ConsoleColor.Red $"%s{name} 不能为空"
+        askToInputValue name
+    | _ -> input
+
+let rec private readSecureValuePaddingWith (p: string) (s: string) =
+    let key = Console.ReadKey(true)
+
+    match key.Key with
+    | ConsoleKey.Enter ->
+        printfn ""
+        s
+    | ConsoleKey.Backspace ->
+        let sn =
+            if s.Length > 0 then
+                let spaces = String.replicate p.Length " "
+                Console.Write($"\b{spaces}\b")
+                s[0 .. s.Length - 2]
+            else
+                s
+
+        readSecureValuePaddingWith p sn
+    | _ ->
+        p |> Console.Write
+        let sn = $"%s{s}%c{key.KeyChar}"
+        readSecureValuePaddingWith p sn
+
+let private readSecureValue (s: string) = readSecureValuePaddingWith "*" s
+
+let rec askToInputSecretValue name =
+    printfn $"请输入{name}： "
+
+    let input = readSecureValuePaddingWith "" String.Empty
+
+    match input with
+    | _ when String.IsNullOrEmpty input ->
+        printfnWithColor ConsoleColor.Red $"{name} 不能为空"
+        askToInputSecretValue name
+    | _ -> input
